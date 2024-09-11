621ef618f74d9de3c6136ccae4ec7c188e179455
tas17 <35601762+tas17@users.noreply.github.com>
2024-09-11 10:49:10 +0200

    Take draws into account

--- main.py
+++ main.py
@@ -23,22 +23,52 @@ def main(infile: str, initializer_file: str | None = None):
     with open(infile) as f:
         lines = f.readlines()
     for line in lines[1:]:
-        winner, looser = line.rstrip('\n').split(SEPARATOR)
-        winner_elo = get_elo(winner)
-        looser_elo = get_elo(looser)
+        p1, p2, score, date = line.rstrip('\n').split(SEPARATOR)
+        if score == "1-0":
+            winner = p1
+            looser = p2
+        elif score == "0-1":
+            winner = p2
+            looser = p1
+        elif score == "1-1":
+            winner = None
+            looser = None
+        else:
+            raise ValueError(f"Unexpcted value for {score=}")
 
-        c_w = 2**(winner_elo)/DEFAULT_ELO
-        c_l = 2**(looser_elo)/DEFAULT_ELO
+        if winner:
+            winner_elo = get_elo(winner)
+            looser_elo = get_elo(looser)
 
-        p_w = c_w / (c_w + c_l)
-        p_l = c_l / (c_w + c_l)
+            c_w = 2**(winner_elo)/DEFAULT_ELO
+            c_l = 2**(looser_elo)/DEFAULT_ELO
 
-        new_winner_elo = winner_elo + 1 - p_w
-        new_looser_elo = looser_elo - p_l
-        print(f"{winner} ({round(winner_elo, ROUNDING)}) wins against {looser} ({round(looser_elo, ROUNDING)}). New elos: {round(new_winner_elo, ROUNDING)} - {round(new_looser_elo, ROUNDING)}")
+            p_w = c_w / (c_w + c_l)
+            p_l = c_l / (c_w + c_l)
 
-        set_elo(winner, new_winner_elo)
-        set_elo(looser, new_looser_elo)
+            new_winner_elo = winner_elo + 1 - p_w
+            new_looser_elo = looser_elo - p_l
+            print(f"{winner} ({round(winner_elo, ROUNDING)}) wins against {looser} ({round(looser_elo, ROUNDING)}). New elos: {round(new_winner_elo, ROUNDING)} - {round(new_looser_elo, ROUNDING)}")
+
+            set_elo(winner, new_winner_elo)
+            set_elo(looser, new_looser_elo)
+        else:
+            p1_elo = get_elo(p1)
+            p2_elo = get_elo(p2)
+
+            c_1 = 2**(p1_elo)/DEFAULT_ELO
+            c_2 = 2**(p2_elo)/DEFAULT_ELO
+
+            p_1 = c_1 / (c_1 + c_2)
+            p_2 = c_2 / (c_1 + c_2)
+
+            new_p1_elo = p1_elo + 0.5 - p_1
+            new_p2_elo = p2_elo + 0.5 - p_2
+
+            print(f"Draw between {p1} ({round(p1_elo, ROUNDING)}) and {p2} ({round(p2_elo, ROUNDING)}). New elos: {round(new_p1_elo, ROUNDING)} - {round(new_p2_elo, ROUNDING)}")
+
+            set_elo(p1, new_p1_elo)
+            set_elo(p2, new_p2_elo)
 
     print("\nRatings")
     for i, (name, elo) in enumerate(sorted(ELOS.items(), key=lambda x: -x[1])):
